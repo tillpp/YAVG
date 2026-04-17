75e2987accde74d4caa73d32b4678183dd32312c
tillpp <tillplusplus@gmail.com>
2026-04-17 10:55:18 +0200

    Just a todo

--- src/graphics/ValidationLayer.hpp
+++ src/graphics/ValidationLayer.hpp
@@ -14,6 +14,7 @@ class ValidationLayer
     const std::vector<char const*> validationLayers = {
         "VK_LAYER_KHRONOS_validation"
     };
+
 #ifdef NDEBUG
     bool enableValidationLayers = false;
 #else
@@ -23,3 +24,5 @@ public:
     ValidationLayer(InstanceSettings& settings);
     ~ValidationLayer();
 };
+
+// TODO: skipped the Message Callback stuff from https://docs.vulkan.org/tutorial/latest/03_Drawing_a_triangle/00_Setup/02_Validation_layers.html
