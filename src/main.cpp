9f81fb374466544ddf76f985f443d39eb6307ac9
tillpp <tilllplusplus@gmail.com>
2026-04-17 02:44:53 +0200

    vulkan

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,11 @@
-
-
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
+#include <vulkan/vulkan.hpp>
 #include <iostream>
+#include <vector>
 
-int main(int argc, char const *argv[])
-{
-    /* code */
+int main() {
+    auto extensions = vk::enumerateInstanceExtensionProperties();
+    for (const auto& ext : extensions) {
+        std::cout << "\t" << ext.extensionName << " version: " << ext.specVersion << "\n";
+    }
     return 0;
 }
