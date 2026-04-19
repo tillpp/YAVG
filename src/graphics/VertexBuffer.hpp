2f9c519ad9731d2cf00516a503cebef3d228e44b
tillpp <tillplusplus@gmail.com>
2026-04-20 00:10:26 +0200

    changed the color of the test rectangle

--- src/graphics/VertexBuffer.hpp
+++ src/graphics/VertexBuffer.hpp
@@ -24,13 +24,15 @@ const std::vector<Vertex> vertices = {
     {{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
     {{0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}},
     {{0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}},
-    {{-0.5f, 0.5f}, {1.0f, 1.0f, 1.0f}}
+    {{-0.5f, 0.5f}, {1.0f, 0.0f, 1.0f}}
 };
 const std::vector<uint16_t> indices = {
     0, 1, 2, 2, 3, 0
 };
 
 //NOTE: there are only 4096 max memory allocations, split bigger buffers into smaller ones with offset. (TODO: custom allocator)
+//NOTE: IndexBuffer and VertexBuffer in one, is more cache friendly.
+//TODO: learn more about "aliasing" in Vulkan
 //TODO move this to Buffer.hpp
 class Buffer{
 public:
