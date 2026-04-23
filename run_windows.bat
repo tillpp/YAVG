61890ff3359dbdc83280dc57c5fdce2547273bf5
tillpp <tillplusplus@gmail.com>
2026-04-23 15:09:53 +0200

    fck window 5

--- run_windows.bat
+++ run_windows.bat
@@ -6,11 +6,11 @@ REM CONFIGURATION
 
 set ROOT=%~dp0compiler\x86_64
 
-set LLVM_VER=18.1.8
+set LLVM_VER=22.1.4
 set CMAKE_VER=3.30.5
 
-set LLVM_URL=https://github.com/llvm/llvm-project/releases/download/llvmorg-%LLVM_VER%/clang+llvm-%LLVM_VER%-x86_64-pc-windows-msvc.zip
-set CMAKE_URL=https://github.com/Kitware/CMake/releases/download/v%CMAKE_VER%/cmake-%CMAKE_VER%-windows-x86_64.zip
+set LLVM_URL=https://github.com/llvm/llvm-project/releases/download/llvmorg-%LLVM_VER%/clang+llvm-%LLVM_VER%-x86_64-pc-windows-msvc.tar.xz
+set CMAKE_URL=https://github.com/Kitware/CMake/releases/download/v%CMAKE_VER%/cmake-%CMAKE_VER%-windows-x86_64.tar.xz
 
 set LLVM_ZIP=%ROOT%\llvm.zip
 set CMAKE_ZIP=%ROOT%\cmake.zip
@@ -20,9 +20,9 @@ echo Creating %ROOT%
 if not exist "%ROOT%" mkdir "%ROOT%"
 
 REM DOWNLOAD CLANG
-echo Downloading Clang and Cmake ....
-powershell -Command $ProgressPreference = 'SilentlyContinue'; "Invoke-WebRequest -uri %LLVM_URL% -OutFile %LLVM_ZIP%"
-powershell -Command $ProgressPreference = 'SilentlyContinue'; "Invoke-WebRequest -uri %CMAKE_URL% -OutFile %CMAKE_ZIP%"
+echo Downloading Clang and Cmake ....%LLVM_URL% into %LLVM_ZIP%
+powershell -Command $ProgressPreference = 'SilentlyContinue'; "Invoke-WebRequest -uri '%LLVM_URL%' -OutFile '%LLVM_ZIP%'"
+powershell -Command $ProgressPreference = 'SilentlyContinue'; "Invoke-WebRequest -uri '%CMAKE_URL%' -OutFile '%CMAKE_ZIP%'"
 echo Extracting zip file...
 C:\Windows\System32\tar.exe -xf %LLVM_ZIP% --directory %ROOT%\llvm
 C:\Windows\System32\tar.exe -xf %CMAKE_ZIP% --directory %ROOT%\cmake
